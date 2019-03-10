20492934628677d35eee1ad1a04c4550b31e3580
Timm Murray <tmurray@wumpus-cave.net>
2019-03-10 12:54:57 -0500

    Make config and set port number from that

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const server = express();
-const PORT = 80;
+const fs = require( 'fs' );
 const pg = require('pg');
+const yaml = require( 'js-yaml' );
+
+var conf = yaml.safeLoad(
+    fs.readFileSync( 'config.yaml', 'utf8' ),
+    {
+        filename: "config.yaml"
+    }
+);
+var PORT = conf.port;
+
+const server = express();
+
 
 server.listen(PORT, () => console.log("Server running"));
 server.get('/', (req, res) => res.status(200).send('hello'));
