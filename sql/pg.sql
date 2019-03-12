e628e0399a120e872295b41bd319ab6c4f976fb4
Timm Murray <tmurray@wumpus-cave.net>
2019-03-11 21:31:37 -0500

    Make table ID attributes consistently named and serial cols. Zip codes should be strings

--- sql/pg.sql
+++ sql/pg.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "members" (
-  "member_id" int,
+  "id" serial primary key,
   "wa_id" int,
   "keyfob_id" int,
   "slack_id" int,
@@ -8,91 +8,93 @@ CREATE TABLE "members" (
   "full_name" varchar,
   "status" bool,
   "city" varchar,
-  "zip" int,
+  "zip" varchar,
   "photo" varchar,
   "member_level" int
 );
 
 CREATE TABLE "member_history" (
-  "id" int,
+  "id" serial primary key,
   "member_id" int,
   "member_start_date" date,
   "member_end_date" date,
-  "member_level" int,
+  "member_level_id" int,
   "status" bool
 );
 
 CREATE TABLE "member_levels" (
-  "level_id" int,
+  "id" serial primary key,
   "level_name" varchar,
   "level_price" int,
   "level_period" varchar
 );
 
 CREATE TABLE "tools" (
-  "tool_id" int,
+  "id" serial primary key,
   "name" varchar,
   "area_id" int,
   "color" varchar,
   "status" varchar,
-  "owner_id" varchar,
+  "owner_id" int,
   "location" int
 );
 
 CREATE TABLE "tool_training" (
-  "member_id" int,
+  "id" serial primary key,
   "tool_id" int,
+  "member_id" int,
   "status" int
 );
 
 CREATE TABLE "tool_usage" (
-  "id" int,
+  "id" serial primary key,
   "tool_id" int,
   "member_id" int,
   "usage_date" date,
   "usage_start" timestamp,
   "usage_stop" timestamp,
-  "usage_duration" double
+  "usage_duration" bigint
 );
 
 CREATE TABLE "areas" (
-  "area_id" int,
+  "id" serial primary key,
   "area_name" varchar
 );
 
 CREATE TABLE "member_roles" (
-  "id" int,
+  "id" serial primary key,
   "member_id" int,
   "area_id" int,
-  "role" int,
+  "role_id" int,
   "specialty" int,
   "start_date" date,
-  "end_Date" date
+  "end_date" date
 );
 
 CREATE TABLE "roles" (
-  "id" int,
+  "id" serial primary key,
+  "role_id" int,
   "role_name" varchar
 );
 
-ALTER TABLE "tools" ADD FOREIGN KEY ("owner_id") REFERENCES "members" ("member_id");
+ALTER TABLE "tools" ADD FOREIGN KEY ("owner_id") REFERENCES "members" ("id");
 
-ALTER TABLE "tools" ADD FOREIGN KEY ("area_id") REFERENCES "areas" ("area_id");
+ALTER TABLE "tools" ADD FOREIGN KEY ("area_id") REFERENCES "areas" ("id");
 
-ALTER TABLE "tool_training" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("member_id");
+ALTER TABLE "tool_training" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("id");
 
-ALTER TABLE "tool_training" ADD FOREIGN KEY ("tool_id") REFERENCES "tools" ("tool_id");
+ALTER TABLE "tool_training" ADD FOREIGN KEY ("tool_id") REFERENCES "tools" ("id");
 
-ALTER TABLE "member_roles" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("member_id");
+ALTER TABLE "member_roles" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("id");
 
-ALTER TABLE "member_roles" ADD FOREIGN KEY ("area_id") REFERENCES "areas" ("area_id");
+ALTER TABLE "member_roles" ADD FOREIGN KEY ("area_id") REFERENCES "areas" ("id");
 
-ALTER TABLE "roles" ADD FOREIGN KEY ("id") REFERENCES "member_roles" ("role");
+ALTER TABLE "roles" ADD FOREIGN KEY ("role_id") REFERENCES "member_roles" ("id");
 
-ALTER TABLE "members" ADD FOREIGN KEY ("member_level") REFERENCES "member_levels" ("level_id");
+ALTER TABLE "members" ADD FOREIGN KEY ("member_level") REFERENCES "member_levels" ("id");
 
-ALTER TABLE "member_history" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("member_id");
+ALTER TABLE "member_history" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("id");
 
-ALTER TABLE "tool_usage" ADD FOREIGN KEY ("tool_id") REFERENCES "tools" ("tool_id");
+ALTER TABLE "tool_usage" ADD FOREIGN KEY ("tool_id") REFERENCES "tools" ("id");
 
-ALTER TABLE "tool_usage" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("member_id");
+ALTER TABLE "tool_usage" ADD FOREIGN KEY ("member_id") REFERENCES "members" ("id");
