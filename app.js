76c8fcabc47f661f1f7642e36053059899ec48f9
Timm Murray <tmurray@wumpus-cave.net>
2019-03-15 16:57:13 -0500

    Break out the implementation functions into a library. Check version response.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require( 'fs' );
 const pg = require('pg');
 const yaml = require( 'js-yaml' );
+const request_funcs = require( './lib/request_funcs.js' );
 
 var conf = yaml.safeLoad(
     fs.readFileSync( 'config.yaml', 'utf8' ),
@@ -14,7 +15,7 @@ var PORT = conf.port;
 const server = express();
 
 
-server.listen(PORT, () => console.log("Server running"));
-server.get('/', (req, res) => res.status(200).send('hello'));
+server.listen(PORT, () => console.log( "Server running on port " + PORT ));
+server.get('/', request_funcs.versions );
 
 module.exports.app = server;
---
+++ test/versions.js
@@ -0,0 +1,25 @@
+var request = require( 'supertest' );
+var server = require( '../app.js' );
+
+const app = server.app;
+
+
+describe( 'Returns version list', function () {
+    it( 'Gets versions', function (done) {
+        request( app )
+            .get( '/' )
+            .expect( 200 )
+            .expect( 'Content-Type', /json/ )
+            .expect( function(res) {
+                var data = res.body;
+                if(! Array.isArray( data ) )
+                    throw new Error( "Didn't return array" );
+                if( "/v1" != data[0] ) throw new Error( "Doesn't list v1" );
+            })
+            .end( function( err, res ) {
+                if( err ) return done(err);
+                done();
+                process.exit();
+            });
+    });
+});
