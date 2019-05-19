d400c27e39c7e0a90d79a9b21c5002c4b791651c
tmurray <tmurray@wumpus-cave.net>
2019-05-19 14:14:05 -0500

    Fetch members from Wild Apricot and display their client IDs

--- src/wild_apricot.ts
+++ src/wild_apricot.ts
@@ -1,3 +1,10 @@
+import * as request from "request";
+
+const wa_domain = "api.wildapricot.org";
+const wa_base_uri = "/v2";
+const wa_oauth_uri = "https://oauth.wildapricot.org/auth/token";
+
+
 export interface WAMember
 {
     wild_apricot_id: number;
@@ -18,6 +25,9 @@ export class WildApricot
     private client_name: string;
     private client_secret: string;
     private account_id: string;
+    private oauth_token: string;
+
+    private contact_uri: string;
 
 
     constructor(
@@ -29,6 +39,13 @@ export class WildApricot
         this.client_name = client_name;
         this.client_secret = client_secret;
         this.account_id = account_id;
+
+        this.contact_uri = "https://"
+            + wa_domain
+            + wa_base_uri
+            + "/Accounts"
+            + "/" + this.account_id
+            + "/Contacts?$async=false";
     }
 
 
@@ -37,6 +54,76 @@ export class WildApricot
         ,error_callback: ( err: Error ) => void
     ): void
     {
-        // TODO
+        // TODO if we get an auth failure, force refetch of oauth token 
+        // and try again
+        let fetch = (oauth_token: string) => {
+            request.get( {
+                url: this.contact_uri,
+                headers: {
+                    Accept: 'application/json'
+                },
+                auth: {
+                    bearer: oauth_token
+                }
+            }, (error, response, body) => {
+                if(! error && response.statusCode == 200 ) {
+                    let parsed_users = JSON.parse( body );
+
+                    let members = parsed_users.Contacts.map( (_) => {
+                        return {
+                            wild_apricot_id: _.Id
+                            ,is_active: _.MembershipEnabled
+                        };
+                    });
+                    success_callback( members );
+                }
+                else {
+                    let err = new Error( "Error fetching WA contact info: "
+                        + error );
+                    error_callback( err );
+                }
+            });
+        };
+
+        this.fetch_oauth_token(
+            fetch
+            ,error_callback
+        );
+    }
+
+
+    private fetch_oauth_token(
+        success_callback: ( token: string ) => void
+        ,error_callback: ( err: Error ) => void
+    )
+    {
+        // TODO save oauth token in a general cache, such as memcached
+        if( this.oauth_token ) {
+            success_callback( this.oauth_token );
+        }
+        else {
+            request.post({
+                url: wa_oauth_uri,
+                auth: {
+                    user: this.client_name
+                    ,pass: this.client_secret
+                    ,sendImmediately: true
+                },
+                form: {
+                    grant_type: 'client_credentials'
+                    ,scope: 'auto'
+                }
+            }, (error, response, body) => {
+                if( (! error) && (response.statusCode == 200) ) {
+                    let parsed_response = JSON.parse( body );
+                    this.oauth_token = parsed_response.access_token;
+                    success_callback( this.oauth_token );
+                }
+                else {
+                    error_callback( new Error( "Error fetching OAuth token"
+                        + " from Wild Apricot: " + error ) );
+                }
+            });
+        }
     }
 }
---
+++ test/get-pending-members-real.ts
@@ -0,0 +1,35 @@
+import * as assert from "assert";
+import * as request from "supertest";
+import * as server from "../app";
+import * as funcs from "../src/request_funcs";
+import * as mock_db from "../src/db-mock";
+import * as wa_api from "../src/wild_apricot_mock";
+
+
+describe( 'GET /v1/members/pending', function() {
+    before( () => {
+        let db = new mock_db.MockDB( null, null );
+        server.start( db );
+    });
+
+    it( 'Gets all pending members on actual Wild Apricot', function(done) {
+        request( server.SERVER )
+            .get( '/api/v1/members/pending' )
+            .send()
+            .expect( 200 )
+            .expect( 'Content-Type', /json/ )
+            .expect( function(res) {
+                let data = res.body;
+                if(! Array.isArray( data ) )
+                    throw new Error( "Didn't return array" );
+            })
+            .end( function( err, res ) {
+                if( err ) return done(err);
+                done();
+            })
+    });
+
+    after( () => {
+        server.stop();
+    });
+});
