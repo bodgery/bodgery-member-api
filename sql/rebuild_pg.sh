2b87d48c9cfa7017e7807f82e66fb50885c00b12
tmurray <tmurray@wumpus-cave.net>
2019-10-06 12:38:22 -0500

    Automatically update TypeORM packages when we rebuild the database

--- sql/rebuild_pg.sh
+++ sql/rebuild_pg.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-DB=$1
+# TODO
+# Whatever tools are used to get the DB user/pass out of the config, we need 
+# to make sure they're available on a wide variety of installations. They 
+# should also parse YAML entirely, rather than this crude method.
+DB=`grep "db_name:" config.yaml | perl -E 'say( (split /:\s*/, <>)[1] )'`
+DB_USER=`grep "db_user:" config.yaml | perl -E 'say( (split /:\s*/, <>)[1] )'`
+DB_PASS=`grep "db_password:" config.yaml | perl -E 'say( (split /:\s*/, <>)[1] )'`
 
 psql -f sql/clear_pg_tables.sql ${DB}
 psql -f sql/pg.sql ${DB}
 #psql -f sql/test_log_data.sql ${DB}
 psql -f node_modules/connect-pg-simple/table.sql ${DB}
+
+npx typeorm-model-generator \
+    -h localhost \
+    -d ${DB} \
+    -u ${DB_USER} \
+    -x ${DB_PASS} \
+    -e postgres \
+    -o src/typeorm \
+    -s public
