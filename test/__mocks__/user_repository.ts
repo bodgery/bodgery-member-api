3c896220980c8a6b7336e21fedbc3842e641fc6c
Kevin Stone <kevinastone@gmail.com>
2020-09-05 22:47:42 -0500

    Removed unused UserRepositoryStub class
    Errantly included in the commit that introduced the test stub for the UserRepository

--- test/__mocks__/user_repository.ts
+++ test/__mocks__/user_repository.ts
@@ -4,17 +4,6 @@ import {users as User} from "../../src/typeorm/entities/users";
 import {UserRepository} from "../../src/typeorm/user_repository";
 
 
-class UserRepositoryStub {
-    private userData: {[username: string]: User};
-
-    addUser(user: User): void {
-        this.userData[user.email] = user;
-    }
-
-
-}
-
-
 export default function() {
 
     const userByEmail: {[username: string]: User} = {};
